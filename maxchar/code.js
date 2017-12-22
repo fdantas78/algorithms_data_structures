63fd2d4d80680a74cfa75e77d90d28e9c88d2510
Fernando Dantas <development@dantas.me>
2017-12-22 20:28:01 +0000

    MaxChar in a string solution

---
+++ maxchar/code.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+    const chars = {};
+    let charMax = '', max = 0;
+    
+    for(let char of str) chars[char] = chars[char] + 1 || 1;
+    
+    for (let char in chars){
+        if (chars[char] > max){
+            charMax = char;
+            max = chars[char];
+        }
+    }
+    
+    return charMax;
+}
+
+console.log(maxChar('Hello there!'));
+
+module.exports = maxChar;
